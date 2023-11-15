2a6d235f0d06bfcec5457e466cb460c93979d60f
Peter Slessing <psless23@student.aau.dk>
2023-11-15 11:12:59 +0100

    Refactoring, fixed incorrect

--- src/graph_theory.h
+++ src/graph_theory.h
@@ -1,17 +1,19 @@
 #ifndef SEM_PROJ_1_GRAPH_THEORY_H
 #define SEM_PROJ_1_GRAPH_THEORY_H
 #include <stdbool.h>
+
+
+struct node {
+    int quality_of_stop;
+    int chargerPresent;
+};
+
 struct edge {
     bool isPresent;
     int traffic;
     int distance;
-};
-
-struct node {
-    struct edge connection1;
-    struct edge connection2;
-    int quality_of_stop;
-    int chargerPresent;
+    struct node connection1;
+    struct node connection2;
 };
 
 
