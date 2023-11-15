1aa5c65bb48ab627e826950e56a5e5cc39f90b0c
Peter Slessing <psless23@student.aau.dk>
2023-11-15 11:03:36 +0100

    Div include statements

--- src/main.c
+++ src/main.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include "graph_theory.h"
+#include "graph_functions.c"
+
+//Here, we want a 2D array featuring the struct implementing how we want to represent the edges of the graph
 
 int main() {
     printf("Hello, KRESTEN!\n");
