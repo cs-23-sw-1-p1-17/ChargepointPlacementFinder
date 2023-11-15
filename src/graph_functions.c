1aa0e2b74ca5b303723b2f7613acd4b309b761de
Peter Slessing <psless23@student.aau.dk>
2023-11-15 10:45:34 +0100

    Oprettelse af div filer, og tilføjelse til cMake

---
+++ src/graph_functions.c
@@ -0,0 +1,3 @@
+//
+// Created by Peter on 15/11/2023.
+//
---
+++ src/graph_theory.h
@@ -0,0 +1,8 @@
+//
+// Created by Peter on 15/11/2023.
+//
+
+#ifndef SEM_PROJ_1_GRAPH_THEORY_H
+#define SEM_PROJ_1_GRAPH_THEORY_H
+
+#endif //SEM_PROJ_1_GRAPH_THEORY_H
