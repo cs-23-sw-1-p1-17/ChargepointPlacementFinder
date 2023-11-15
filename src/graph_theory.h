79fb556569750b2545d07b4984968e3c4081e6a3
Peter Slessing <psless23@student.aau.dk>
2023-11-15 11:03:20 +0100

    Header file now includes structs for both nodes and edges

--- src/graph_theory.h
+++ src/graph_theory.h
@@ -1,8 +1,18 @@
-//
-// Created by Peter on 15/11/2023.
-//
-
 #ifndef SEM_PROJ_1_GRAPH_THEORY_H
 #define SEM_PROJ_1_GRAPH_THEORY_H
+
+struct edge {
+    int isPresent;
+    int traffic;
+    int distance;
+};
+
+struct node {
+    struct edge connection1;
+    struct edge connection2;
+    int quality_of_stop;
+    int chargerPresent;
+};
+
 
 #endif //SEM_PROJ_1_GRAPH_THEORY_H
