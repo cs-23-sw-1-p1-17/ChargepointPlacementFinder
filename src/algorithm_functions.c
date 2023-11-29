e835e2530fcd7dd6d8bea784e9c8d33e9fb1d461
Kresten Laust <krestenlaust@gmail.com>
2023-11-29 14:46:25 +0100

    Improved readability, and reformatted code.

--- src/algorithm_functions.c
+++ src/algorithm_functions.c
@@ -1,63 +1,64 @@
 #include "algorithm_functions.h"
 
 solution_node_t* create_solution_array(node_t* node_list, int node_list_length, edge_t* edge_matrix) {
-    //Create solution node array
+    // Create solution node array
     solution_node_t *solution_array = (solution_node_t *) malloc(sizeof(solution_node_t) * node_list_length);
     solution_node_t current_solution_node;
 
     for (int i = 0; i < node_list_length; i++) {
-
-        //Code for the primary nodes
+        // Code for the primary nodes
         int total_traffic_attracted = 0;
         for (int j = 0; j < node_list_length; j++) {
-            if (edge_matrix[get_cell(node_list_length, i+1, j+1)].is_present) {
-                total_traffic_attracted += edge_matrix[get_cell(node_list_length, i+1, j+1)].traffic;
+            edge_t edge = edge_matrix[get_cell(node_list_length, i+1, j+1)];
+
+            if (edge.is_present) {
+                total_traffic_attracted += edge.traffic;
             }
         }
 
-        //TotalTrafficAttracted now contains all the primary edges
+        // TotalTrafficAttracted now contains all the primary edges
 
 
         int secondary_node_traffic = 0;
         node_t *current_secondary_node = NULL;
 
-        //Running through connection matrix
-        //Still on primary node
-        //This loop finds the connected node from the edge
+        // Running through connection matrix
+        // Still on primary node
+        // This loop finds the connected node from the edge
         for (int j = 0; j < node_list_length; j++) {
             secondary_node_traffic = 0;
+            edge_t current_edge = edge_matrix[get_cell(node_list_length, i + 1, j + 1)];
 
-            if (edge_matrix[get_cell(node_list_length, i+1, j+1)].is_present) {
-                current_secondary_node = get_connected_node(&node_list[i], edge_matrix[get_cell(node_list_length, i+1, j+1)]);
+            if (current_edge.is_present) {
+                current_secondary_node = get_connected_node(&node_list[i], current_edge);
                 for (int k = 0; k < node_list_length; k++) {
                     secondary_node_traffic += edge_matrix[get_cell(node_list_length, current_secondary_node->node_id,k + 1)].traffic;
                 }
-                secondary_node_traffic -= edge_matrix[get_cell(node_list_length, i + 1, j + 1)].traffic;
 
+                secondary_node_traffic -= current_edge.traffic;
                 secondary_node_traffic *= get_secondary_factor(node_list[i].quality_of_stop,
                                                                current_secondary_node->quality_of_stop);
                 total_traffic_attracted += secondary_node_traffic;
             }
-
         }
-//Save current traffic attracted in some form of array
+
+        // Save current traffic attracted in some form of array
         current_solution_node.node_ID = i + 1;
         current_solution_node.total_traffic_attracted = total_traffic_attracted;
 
         solution_array[i] = current_solution_node;
-
     }
+
     return solution_array;
 }
 
 node_t* get_connected_node (node_t* current_node, edge_t current_edge) {
-    if(current_edge.connection1 == current_node) { return current_edge.connection2;
-    } return current_edge.connection1;
+    return current_edge.connection1 == current_node ? current_edge.connection2 : current_edge.connection1;
 }
 
-int compare_solution_node(const void* a, const void* b) {
-    solution_node_t a1 = *(solution_node_t*) a;
-    solution_node_t b1 = *(solution_node_t*) b;
+int compare_solution_node(const void* solution_node_a, const void* solution_node_b) {
+    solution_node_t a1 = *(solution_node_t*) solution_node_a;
+    solution_node_t b1 = *(solution_node_t*) solution_node_b;
     return b1.total_traffic_attracted - a1.total_traffic_attracted;
 }
 
@@ -72,31 +73,22 @@ double get_secondary_factor(int primary_node_quality, int secondary_node_quality
     {
         case 4:
             return POSITIVE_FOUR;
-
         case 3:
             return POSITIVE_THREE;
-
         case 2:
             return POSITIVE_TWO;
-
         case 1:
             return POSITIVE_ONE;
-
         case 0:
             return 0;
-
         case -1:
             return NEGATIVE_ONE;
-
         case -2:
             return NEGATIVE_TWO;
-
         case -3:
             return NEGATIVE_THREE;
-
         case -4:
             return NEGATIVE_FOUR;
-
         default:
             printf("Something went wrong in relative factor calculation...");
             return 0;
--- src/algorithm_functions.h
+++ src/algorithm_functions.h
@@ -17,7 +17,7 @@ solution_node_t* create_solution_array(node_t* node_list, int node_list_length, 
 
 node_t* get_connected_node (node_t* current_node, edge_t current_edge);
 
-int compare_solution_node(const void* a, const void* b);
+int compare_solution_node(const void* solution_node_a, const void* solution_node_b);
 
 void sort_solution_node_array(solution_node_t* solution_nodes, int length_of_solution_nodes);
 
