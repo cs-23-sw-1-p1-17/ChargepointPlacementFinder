e2a4cbbd9c9333ef4d77c778462acb89ba0ad453
Sebastian L <61430494+Naitsabot@users.noreply.github.com>
2023-12-01 13:44:15 +0100

    Update src/algorithm_functions.c

--- src/algorithm_functions.c
+++ src/algorithm_functions.c
@@ -68,7 +68,7 @@ void sort_solution_node_array(solution_node_t* solution_nodes, int length_of_sol
 
 double get_secondary_factor(int primary_node_quality, int secondary_node_quality) {
     int relative_factor = primary_node_quality - secondary_node_quality;
-    double factor_array[9] = {NEGATIVE_FOUR, NEGATIVE_THREE, NEGATIVE_TWO, NEGATIVE_ONE, 0, POSITIVE_ONE, POSITIVE_TWO, POSITIVE_THREE, POSITIVE_FOUR};
+    double factor_array[] = {NEGATIVE_FOUR, NEGATIVE_THREE, NEGATIVE_TWO, NEGATIVE_ONE, 0, POSITIVE_ONE, POSITIVE_TWO, POSITIVE_THREE, POSITIVE_FOUR};
     return factor_array[relative_factor + 4];
 
 }
