544744a7f41d5261325eb36b6dad9c247d84d94f
DigitaWings <45568915+DigitaWings@users.noreply.github.com>
2023-12-06 16:19:23 +0100

    Refactors get_cell to be 0 indexed, and refactors solution alg to accomodate said refactoring

--- src/algorithm_functions.c
+++ src/algorithm_functions.c
@@ -9,7 +9,7 @@ solution_node_t* create_solution_array(node_t* node_list, int node_list_length, 
         // Code for the primary nodes
         int total_traffic_attracted = 0;
         for (int j = 0; j < node_list_length; j++) {
-            edge_t edge = edge_matrix[get_cell(node_list_length, i+1, j+1)];
+            edge_t edge = edge_matrix[get_cell(node_list_length, i, j)];
 
             if (edge.is_present) {
                 total_traffic_attracted += edge.traffic;
@@ -26,15 +26,14 @@ solution_node_t* create_solution_array(node_t* node_list, int node_list_length, 
         // This loop finds the connected node from the edge
         for (int j = 0; j < node_list_length; j++) {
             secondary_node_traffic = 0;
-            edge_t current_edge = edge_matrix[get_cell(node_list_length, i + 1, j + 1)];
 
-            if (current_edge.is_present) {
-                current_secondary_node = get_connected_node(&node_list[i], current_edge);
+            if (edge_matrix[get_cell(node_list_length, i, j)].is_present) {
+                current_secondary_node = &node_list[j];
                 for (int k = 0; k < node_list_length; k++) {
-                    secondary_node_traffic += edge_matrix[get_cell(node_list_length, current_secondary_node->node_id,k + 1)].traffic;
+                    secondary_node_traffic += edge_matrix[get_cell(node_list_length, j, k)].traffic;
                 }
 
-                secondary_node_traffic -= current_edge.traffic;
+                secondary_node_traffic -= edge_matrix[get_cell(node_list_length, i , j)].traffic;
                 secondary_node_traffic = floor(secondary_node_traffic * get_secondary_factor(node_list[i].quality_of_stop,
                                                                current_secondary_node->quality_of_stop));
                 total_traffic_attracted += secondary_node_traffic;
@@ -42,7 +41,7 @@ solution_node_t* create_solution_array(node_t* node_list, int node_list_length, 
         }
 
         // Save current traffic attracted in some form of array
-        current_solution_node.node_ID = i + 1;
+        current_solution_node.node_ID =  i + 1;
         current_solution_node.total_traffic_attracted = total_traffic_attracted;
 
         solution_array[i] = current_solution_node;
