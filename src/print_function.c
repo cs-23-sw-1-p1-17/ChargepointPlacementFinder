d67b687f3356a0cfadd0b97c9af477bd8edafbe2
Sebastian L <61430494+Naitsabot@users.noreply.github.com>
2023-12-08 11:50:13 +0100

    Update src/print_function.c

--- src/print_function.c
+++ src/print_function.c
@@ -9,7 +9,7 @@ void print_solution_nodes(solution_node_t* solution_nodes, int length_of_solutio
 
     for (int i = 0; i < length_of_solution_nodes; i++)
     {
-        printf("%6d. | %5d%2c | %11d\n",i+1, solution_nodes[i].node_ID, ' ', solution_nodes[i].total_traffic_attracted);
+        printf("%6d. | %5d%2c | %11d\n", i+1, solution_nodes[i].node_ID, ' ', solution_nodes[i].total_traffic_attracted);
     }
 
 }
