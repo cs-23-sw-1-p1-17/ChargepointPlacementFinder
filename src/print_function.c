26ace5d9422fa523a385e8a9293393da4e63f441
pSlessing <144026479+pSlessing@users.noreply.github.com>
2023-12-11 10:57:29 +0100

    Formatting
    Co-authored-by: Kresten Laust <krestenlaust@gmail.com>

--- src/print_function.c
+++ src/print_function.c
@@ -6,7 +6,6 @@ void print_solution_nodes(solution_node_t* solution_nodes, int length_of_solutio
     printf(" Ranked\t| Node ID | Traffic attracted\n");
     printf("--------------------------------------\n");
 
-
     for (int i = 0; i < length_of_solution_nodes; i++)
     {
         printf("%6d. | %5d%2c | %11d\n", i+1, solution_nodes[i].node_ID, ' ', solution_nodes[i].total_traffic_attracted);
