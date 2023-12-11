78244a94e11e1422a3588d3d51e3285ff638d6af
pSlessing <144026479+pSlessing@users.noreply.github.com>
2023-12-11 10:57:51 +0100

    Formatting
    Co-authored-by: Kresten Laust <krestenlaust@gmail.com>

--- src/main.c
+++ src/main.c
@@ -9,7 +9,6 @@
 #define AMOUNT_NODE 10
 
 int main() {
-
     node_t* nodes = create_sample_network_nodes();
     edge_t* edges = create_sample_network_edge_matrix();
     solution_node_t* solution_nodes = create_solution_array(nodes, TEST_NETWORK_NODE_AMOUNT, edges);
