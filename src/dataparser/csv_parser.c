56cdc43ef7dc23435d6f31a8d6f076ccbe167858
naitsa <naitsabot@gmail.com>
2023-12-12 11:03:32 +0100

    now it actually works

--- src/dataparser/csv_parser.c
+++ src/dataparser/csv_parser.c
@@ -1,33 +1,37 @@
 #include "csv_parser.h"
 
-void parse_nodes(node_t *nodes) {
+void parse_nodes(const char *filepath, node_t *nodes) {
     // open file with nodes
-    FILE *in_fp = NULL;
-    in_fp = fopen(NODE_LIST_FILEPATH, "r");
-    if(in_fp == NULL) {
-        printf("Error!");
+    FILE *input_fp = NULL;
+    input_fp = fopen(filepath, "r");
+    // if file not opened
+    if(input_fp == NULL) {
+        printf("Error Loading Nodes!");
         exit(EXIT_FAILURE);
     }
 
     for (int i = 0; i < NUM_EXAMPLE_NODES; ++i) {
         // creating temp variables
         node_t node;
+
         // get values from string
-        int rt = fscanf(in_fp, " %[^;];%d;%d;%lf%*[^ \\t\\n]",
+        int rt = fscanf(input_fp, " %[^;];%d;%d;%lf*[^ \\t\\n]",
                         node.name, &node.node_id, &node.charger_present, &node.quality_of_stop);
-        // assign if successfully parsed
+
+        // assign to if successfully parsed
         if (rt == NUM_EXAMPLE_NODE_PARSED_VALUES){
             nodes[i] = node;
         }
     }
 }
 
-void parse_edges(edge_t *edges, node_t *nodes) {
+void parse_edges(const char *filepath, edge_t *edges, node_t *nodes) {
     // open file with edges
-    FILE *in_fp = NULL;
-    in_fp = fopen(EDGE_LIST_FILEPATH, "r");
-    if(in_fp == NULL) {
-        printf("Error!");
+    FILE *input_fp = NULL;
+    input_fp = fopen(filepath, "r");
+    // if file not opened
+    if(input_fp == NULL) {
+        printf("Error Loading Edges!");
         exit(EXIT_FAILURE);
     }
 
@@ -36,12 +40,15 @@ void parse_edges(edge_t *edges, node_t *nodes) {
         edge_t edge;
         int node_a;
         int node_b;
+
         // get values from string
-        int rt = fscanf(in_fp, " %[^;]s;%d;%lf;%d;%d%*[^ \\t\\n]",
+        int rt = fscanf(input_fp, " %[^;];%d;%lf;%d;%d*[^ \\t\\n]",
                         edge.name, &edge.traffic, &edge.distance, &node_a, &node_b);
+
         // assign connecting nodes
         edge.connection1 = &nodes[node_a];
         edge.connection2 = &nodes[node_b];
+
         // assign if successfully parsed
         if (rt == NUM_EXAMPLE_EDGE_PARSED_VALUES) {
             edges[i] = edge;
@@ -49,25 +56,28 @@ void parse_edges(edge_t *edges, node_t *nodes) {
     }
 }
 
-void example_parsing_print(edge_t *edges) {
+void parsing_debug_print(edge_t *edges) {
     for (int i = 0; i < NUM_EXAMPLE_EDGES; ++i) {
-        printf("E-name: %s\nE-pres: %d\nE-traf: %d\nE-dist: %lf\n",
-               edges[i].name, edges[i].is_present, edges[i].traffic, edges[i].distance);
-        printf("->n1-name: %s\n->n1-id:  %d\n->n1-qual: %lf\n->n1-char: %d\n",
+        printf("::: %d :::\nE-name:       %s\nE-traffic:    %d\nE-distance:   %lf\n",
+               i+1, edges[i].name, edges[i].traffic, edges[i].distance);
+        printf("->n1-name:    %s\n->n1-id:      %d\n->n1-quality: %lf\n->n1-charger: %d\n",
                edges[i].connection1->name, edges[i].connection1->node_id,edges[i].connection1->quality_of_stop,edges[i].connection1->charger_present);
-        printf("->n2-name: %s\n->n2-id:  %d\n->n2-qual: %lf\n->n2-char: %d\n",
+        printf("->n2-name:    %s\n->n2-id:      %d\n->n2-quality: %lf\n->n2-charger: %d\n",
                edges[i].connection2->name, edges[i].connection2->node_id,edges[i].connection2->quality_of_stop,edges[i].connection2->charger_present);
         printf("\n");
     }
 }
 
-edge_t* example_parsing() {
+void parsing() {
+    printf("hello\n");
+
     node_t nodes[NUM_EXAMPLE_NODES];
-    parse_nodes(nodes);
+    parse_nodes(NODE_LIST_FILEPATH, nodes);
 
     edge_t edges[NUM_EXAMPLE_EDGES];
-    parse_edges(edges, nodes);
+    parse_edges(EDGE_LIST_FILEPATH, edges, nodes);
+    printf("HOOOOOOLAAAAA\n");
 
-    example_parsing_print(edges);
+    parsing_debug_print(edges);
 }
 
--- src/dataparser/csv_parser.h
+++ src/dataparser/csv_parser.h
@@ -2,16 +2,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "graph_functions.h"
 
 #define NUM_EXAMPLE_NODES 16
 #define NUM_EXAMPLE_EDGES 20
 #define NUM_EXAMPLE_NODE_PARSED_VALUES 4
 #define NUM_EXAMPLE_EDGE_PARSED_VALUES 5
-#define NODE_LIST_FILEPATH ("../src/dataparser/node_list.csv")
-#define EDGE_LIST_FILEPATH ("../src/dataparser/edge_list.csv")
+#define NODE_LIST_FILEPATH ("src/dataparser/node_list.csv")
+#define EDGE_LIST_FILEPATH ("src/dataparser/edge_list.csv")
 
-void parse_nodes(node_t *nodes);
-void parse_edges(edge_t *edges, node_t *nodes);
-edge_t* example_parsing();
-void example_parsing_print(edge_t *edges);
+void parse_nodes(const char *filepath, node_t *nodes);
+void parse_edges(const char *filepath, edge_t *edges, node_t *nodes);
+void parsing_debug_print(edge_t *edges);
+void parsing();
