31b18a400074285965b75c3432c9605d033d875a
Kresten Laust <krestenlaust@gmail.com>
2023-12-13 15:51:44 +0100

    Resolved various issues

--- src/dataparser/csv_parser.c
+++ src/dataparser/csv_parser.c
@@ -15,11 +15,11 @@ void parse_nodes(const char *filepath, node_t *nodes, int n) {
         node_t node;
 
         // get values from string
-        int rt = fscanf(input_fp, " %[^;];%d;%d;%lf*[^ \\t\\n]",
-                        node.name, &node.node_id, &node.charger_present, &node.quality_of_stop);
+        int assigned_fields_count = fscanf(input_fp, " %[^;];%d;%d;%lf*[^ \\t\\n]",
+                                           node.name, &node.node_id, &node.charger_present, &node.quality_of_stop);
 
         // assign to if successfully parsed
-        if (rt == EXPECTED_PARSED_VALUES_PR_NODE){
+        if (assigned_fields_count == EXPECTED_PARSED_VALUES_PR_NODE){
             nodes[i] = node;
         }
     }
@@ -38,19 +38,19 @@ void parse_edges(const char *filepath, edge_t *edges, node_t *nodes, int n) {
     for (int i = 0; i < n; ++i) {
         // creating temp variables
         edge_t edge;
-        int node_a;
-        int node_b;
+        int node_a = 0;
+        int node_b = 0;
 
         // get values from string
-        int rt = fscanf(input_fp, " %[^;];%d;%lf;%d;%d*[^ \\t\\n]",
-                        edge.name, &edge.traffic, &edge.distance, &node_a, &node_b);
+        int assigned_fields_count = fscanf(input_fp, " %[^;];%d;%lf;%d;%d*[^ \\t\\n]",
+                                           edge.name, &edge.traffic, &edge.distance, &node_a, &node_b);
 
         // assign connecting nodes
-        edge.connection1 = &nodes[node_a-1]; // minus one as list is 1-index but array is 0-indexed
+        edge.connection1 = &nodes[node_a-1]; // TODO: minus one as list is 1-index but array is 0-indexed
         edge.connection2 = &nodes[node_b-1];
 
         // assign if successfully parsed
-        if (rt == EXPECTED_PARSED_VALUES_PR_EDGE) {
+        if (assigned_fields_count == EXPECTED_PARSED_VALUES_PR_EDGE) {
             edges[i] = edge;
         }
     }
