57b371a6f3dfd65b6b8bab513875777cb554dc57
naitsa <naitsabot@gmail.com>
2023-12-15 13:55:26 +0100

    correct index for naming

--- src/console_output/print_function.c
+++ src/console_output/print_function.c
@@ -6,6 +6,6 @@ void print_solution_nodes(solution_node_t *solution_nodes, int length_of_solutio
 
     for (int i = 0; i < length_of_solution_nodes; i++) {
         printf("%6d. | %5d%2c | %11d | %s\n", i + 1, solution_nodes[i].node_id, ' ',
-               solution_nodes[i].total_traffic_attracted, node_list[i].name);
+               solution_nodes[i].total_traffic_attracted, node_list[solution_nodes[i].node_id-1].name);
     }
 }
