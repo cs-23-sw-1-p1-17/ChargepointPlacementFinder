e695271c90feadb98fb41cc2bd1021f902a15229
Amalie <agje23@student.aau.dk>
2023-12-18 12:40:26 +0100

    fixed a few things

--- src/console_output/print_function.c
+++ src/console_output/print_function.c
@@ -1,13 +1,16 @@
 #include "print_function.h"
 
 void print_solution_nodes(solution_node_t *solution_nodes, int length_of_solution_nodes, node_t *node_list) {
-    int longest_name_length;
+    int longest_name_length = 0;
+    int name_length;
     for (int i = 0; i < length_of_solution_nodes; ++i) {
-        if (strlen(node_list[solution_nodes[i].node_id-1].name) > longest_name_length){
-            longest_name_length = strlen(node_list[solution_nodes[i].node_id-1].name);
+        name_length = strlen(node_list[solution_nodes[i].node_id-1].name);
+        if (name_length > longest_name_length){
+            longest_name_length = name_length;
         }
     }
 
+    // padding on both sides of 'Name' is half of the longest item in that column. It keeps the title centered
     printf(" Ranked\t| Node ID | %*c Name %*c | Traffic attracted\n", (longest_name_length/2) - 2, ' ', (longest_name_length/2) - 3, ' ');
 
     for (int i = 0; i < longest_name_length; ++i) {
